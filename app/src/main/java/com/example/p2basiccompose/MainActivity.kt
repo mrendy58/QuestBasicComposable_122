005401b26b58adbee7bd90f51b3980300113419b
mrendy58 <derendyyy58@gmail.com>
2024-10-16 20:59:01 +0700

    menambah text nama

--- app/src/main/java/com/example/p2basiccompose/MainActivity.kt
+++ app/src/main/java/com/example/p2basiccompose/MainActivity.kt
@@ -59,6 +59,7 @@ fun BasicLayout(modifier: Modifier = Modifier) {
             contentDescription =null,
             modifier = Modifier.padding(top = 40.dp).size(250.dp)
         )
+        Text(text = "nama",)
     }
 
 }
